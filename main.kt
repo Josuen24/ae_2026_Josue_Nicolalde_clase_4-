ffacc6bd74e0a4345f3a38d52f139b444cdf3218
Estudiante LTIC <Estudiante@ltic.local>
2026-05-16 11:51:04 -0500

    clase_4{{1.1}}

---
+++ main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var mensaje: String = "Hola mundo"
+    var variableMutable: Int = 5
+    println(mensaje)
+    println(variableMutable)
+    
+    variableMutable = 10
+    println(variableMutable)
+    
+    mensaje = "hola mundo mutado"
+	println(mensaje)
+    
+ }
