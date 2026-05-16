fdfe89325e0f1dd4df867977a2cf4ff00e85b8af
Estudiante LTIC <Estudiante@ltic.local>
2026-05-16 11:52:56 -0500

    clase_4{{5}}

--- main.kt
+++ main.kt
@@ -1,60 +1,68 @@
 data class Student(
-	val id: Long,
+    val id: Long,
     val name: String,
     val email: String,
     val grade: Int,
     val isActive: Boolean
 )
-fun getStidents(): List<Student>{
-    val Jorge = Student(
+
+fun getStudents(): List<Student> {
+
+    val jorge = Student(
+        id = 1,
         name = "Jorge",
         email = "jorge@puce.edu.ec",
         grade = 8,
-        id = 1,
         isActive = true
-   ) 
-    val Ana = Student(
+    )
+
+    val ana = Student(
+        id = 2,
         name = "Ana",
-        email = "jorge@puce.edu.ec",
-        grade = 8,
-        id = 1,
+        email = "ana@puce.edu.ec",
+        grade = 6,
         isActive = true
-   ) 
-    val Juan = Student(
+    )
+
+    val juan = Student(
+        id = 3,
         name = "Juan",
-        email = "jorge@puce.edu.ec",
-        grade = 8,
-        id = 1,
+        email = "juan@puce.edu.ec",
+        grade = 9,
         isActive = true
-   ) 
-    return listOf(Jorge, Ana, Juan)
+    )
+
+    return listOf(jorge, ana, juan)
 }
+
 fun getResult(grade: Int): String {
-    return if  (grade>7){
+    return if (grade > 7) {
         "Aprobado"
     } else {
         "Reprobado"
     }
 }
 
-fun main(){
-    for (Studentt in getStudent()){
-        println("${student.name} esta ${getResult{student.grade}}")
-    }
-}
-
 fun main() {
-	val Jorge = Student(
-        name = "Jorge",
-        email = "jorge@puce.edu.ec",
-        grade = 8,
-        id = 1,
-        isActive = true
-   ) 
-    if(Jorge.grade > 7){
-        println("Aprobado")
-    } else {
-        println("reprobado")
+
+    val estudiantesEstudiosos = mutableListOf<Student>()
+
+    for (student in getStudents()) {
+
+        if (student.grade > 7) {
+            estudiantesEstudiosos.add(student)
+        }
+
+        println("${student.name}: ${getResult(student.grade)}")
     }
-    println(Jorge)
+
+    println("Estudiantes estudiosos:")
+    val estudiantesEstudiosos2: List<Student> = getStudents().filter { loQueSea ->
+    	loQueSea.grade > 7
+    }
+    println(estudiantesEstudiosos2)
+
+    for (student in estudiantesEstudiosos) {
+        println(student)
+    }
 }
